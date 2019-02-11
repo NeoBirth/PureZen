e3d87c3d9070bcf64796db12a7accf0d25ce8f8d
Tony Arcieri <bascule@gmail.com>
2019-02-11 11:18:05 -0800

    src/utils.rs: Translate from original ZenGarden C++ source

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,33 @@
 //! implemented as an extensible audio library allowing full control over
 //! signal processing, message passing, and graph manipulation.
 //!
+//! Adapted from [ZenGarden] (originally written in C++) with intent to target
+//! embedded (i.e. `no_std`) platforms.
+//!
 //! [Pure Data]: https://puredata.info/
+//! [ZenGarden]: https://github.com/mhroth/ZenGarden
+
+//
+// Copyright © 2019 NeoBirth Developers
+//
+// This file is part of PureZen (a fork of ZenGarden)
+//
+// PureZen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PureZen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with PureZen.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #![no_std]
+#![cfg_attr(all(feature = "nightly", not(feature = "std")), feature(alloc))]
 #![deny(
     warnings,
     missing_docs,
@@ -14,3 +38,12 @@
 )]
 #![forbid(unsafe_code)]
 #![doc(html_root_url = "https://docs.rs/purezen/0.0.0")]
+
+#[cfg(feature = "std")]
+extern crate std;
+
+// TODO: remove `pub` on modules which are not part of the public API
+#[cfg(feature = "alloc")]
+mod prelude;
+#[cfg(feature = "std")]
+pub mod utils;
--- src/utils.rs
+++ src/utils.rs
@@ -1,116 +1,68 @@
-/*
- *  Copyright 2009,2011 Reality Jockey, Ltd.
- *                 info@rjdj.me
- *                 http://rjdj.me/
- * 
- *  This file is part of ZenGarden.
- *
- *  ZenGarden is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  ZenGarden is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with ZenGarden.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+//
+// Copyright © 2009-2019 NeoBirth Developers, Reality Jockey, Ltd.
+//
+// This file is part of PureZen (a fork of ZenGarden)
+//
+// PureZen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PureZen is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with PureZen.  If not, see <http://www.gnu.org/licenses/>.
+//
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "regex.h"
-#include "utils.h"
+//! Legacy utility functions
+//!
+//! These should eventually be replaced with Rust `core` and `std` functionality
 
-utils::utils() {
-  // nothing to do
+#[cfg(feature = "alloc")]
+use crate::prelude::*;
+use core::f32::consts::PI;
+
+/// Legacy string copying function
+// TODO: replace all usages with `to_owned()` once all C++ is translated
+#[cfg(feature = "alloc")]
+pub fn copy_string(s: &str) -> String {
+    s.to_owned()
 }
 
-utils::~utils() {
-  // nothing to do
+/// Legacy check for if value is numeric
+// TODO: replace all usages with `parse::<f32>` once all C++ is translated
+pub fn is_numeric(s: &str) -> bool {
+    s.parse::<f32>().is_ok()
 }
 
-char *utils::copy_string(const char *str) {
-  if (str == NULL) {
-    return NULL;
-  } else {
-    char *newStr = (char *) malloc((strlen(str)+1) * sizeof(char));
-    strcpy(newStr, str);
-    return newStr;
-  }
+/// Legacy string concatenation function
+// TODO: replace all usages with `+` once all C++ is translated
+#[cfg(feature = "alloc")]
+pub fn concat_strings(s0: &str, s1: &str) -> String {
+    s0.to_owned() + s1
 }
 
-bool utils::is_numeric(const char *str) {
-  // http://www.regular-expressions.info/floatingpoint.html
-  regex_t preg;
-  regcomp(&preg, "^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$", REG_NOSUB | REG_EXTENDED);
-  bool isFloat = (regexec(&preg, str, 0, NULL, 0) == 0);
-  regfree(&preg);
-  return isFloat;
+/// Legacy since approximation function
+// TODO: replace all usages with `f32::sin`?
+pub fn sine_approx(x: f32) -> f32 {
+    let a = 4.0f32 / PI; // 1.273239544735163
+    let b = 4.0f32 / (PI * PI); // 0.405284734569351
+    (a * x) - (b * x) * x.abs()
 }
 
-char *utils::concat_strings(const char *path0, const char *path1) {
-  if (path0 == NULL || path1 == NULL) {
-    return NULL;
-  } else {
-    char *joinedPaths = (char *) malloc((strlen(path0) + strlen(path1) + 1) * sizeof(char));
-    strcpy(joinedPaths, path0);
-    strcat(joinedPaths, path1);
-    return joinedPaths;
-  }
+/// Legacy string tokenization function
+// TODO: replace all usages with `core::str::Split`
+#[cfg(feature = "alloc")]
+pub fn tokenize_string<'a>(input: &'a str, delim: &str) -> Vec<&'a str> {
+    input.split(delim).collect()
 }
 
-float utils::sine_approx(float x) {
-  const static float a = 4.0f / M_PI; // 1.273239544735163
-  const static float b = 4.0f / (M_PI * M_PI); // 0.405284734569351
-  return (a * x) - (b * x) * fabsf(x);
-  // NOTE(mhroth): use for former method instead of the latter for now (unless performance issues arise)
-  // in order to avoid type-punning pointer warnings.
-  //int y = *(int *)&x | 0x80000000;
-  //return (1.273239544735163f * x) - (0.405284734569351f * x) * (*(float *)&y);
-}
-
-vector<string> utils::tokenize_string(const char *str, const char *delim) {
-  vector<string> tokenizedStrings = vector<string>();
-  string s0 = string(str);
-  
-  const char *head = str;
-  char *tail = NULL;
-  while ((tail = strstr(head, delim)) != NULL) {
-    int numBytes = tail-head;
-    string nextToken = string(s0, head-str, numBytes);
-    tokenizedStrings.push_back(nextToken);
-    head = tail + strlen(delim);
-  }
-  if (head < str + strlen(str)) {
-    int numBytes = str + strlen(str) - head;
-    string nextToken = string(s0, head-str, numBytes);
-    tokenizedStrings.push_back(nextToken);
-  }
-  return tokenizedStrings;
-}
-
-const char *utils::message_element_type_to_string(MessageElementType type) {
-  switch (type) {
-    case FLOAT: return "FLOAT";
-    case SYMBOL: return "SYMBOL";
-    case BANG: return "BANG";
-    case ANYTHING: return "ANYTHING";
-    case LIST: return "LIST";
-    default: return "UNKNOWN TYPE";
-  }
-}
-
-bool utils::file_exists(const char *path) {
-  FILE *fp = fopen(path, "r");
-  if (fp != NULL) {
-    fclose(fp);
-    return true;
-  }
-  return false;
+/// Legacy file exists function
+// TODO: replace all usages with `Path::exists`
+#[cfg(feature = "std")]
+pub fn file_exists(path: &str) -> bool {
+    std::path::Path::new(path).exists()
 }
